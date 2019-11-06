be66e65d6d0aec1000ed8226ee02521c4383876e
Kevin Krato <kratok08@gmail.com>
2019-11-06 13:39:42 -0700

    Complete iteration 1

---
+++ test/Ingredient_test.rb
@@ -0,0 +1,20 @@
+require "minitest"
+require "minitest/autorun"
+require "minitest/pride"
+require_relative "../lib/Ingredient.rb"
+
+class IngredientTest < Minitest::Test
+  def setup
+    @ingredient = Ingredient.new("Cheese", "oz", "calories")
+  end
+
+  def test_it_exists
+    assert_instance_of Ingredient, @ingredient
+  end
+
+  def test_it_has_attributes
+    assert_equal "Cheese", @ingredient.name
+    assert_equal "oz", @ingredient.unit
+    assert_equal "calories", @ingredient.calories
+  end
+end
